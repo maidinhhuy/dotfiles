45546a083b50f8f1e5e026ca47478b15e9f76a19
richardmai <richardmai@debian-huy.168.1.10.non-exists.ptr.local>
2024-11-30 22:22:46 -0800

    fix: jdtls

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,1 +1,14 @@
-return {}
+return {
+  "neovim/nvim-lspconfig",
+  opts = {
+    -- make sure mason installs the server
+    servers = {
+      jdtls = {},
+    },
+    setup = {
+      jdtls = function()
+        return true -- avoid duplicate servers
+      end,
+    },
+  },
+}
